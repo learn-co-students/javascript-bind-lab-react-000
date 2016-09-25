a96c6ebec561366dc69bd0a849799221331be7c7
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-09-25 09:53:04 -0400

    digitialClock with arrow function

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much.";
+
+const digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  startTicking: function() {
+    setInterval(() => {
+      this.time ++;
+    }, 1000)
+  }
+}
