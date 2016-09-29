23ed51c1c4c1eaadd792e323ac46fd43b1681bab
Luke Ghenco <lukeghenco@gmail.com>
2016-09-29 05:53:02 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const app = "I don't do much.";
+const digitalClock = {
+    time: Math.round(Date.now() / 1000),
+    startTicking: function() {
+        setInterval(() => this.time++, 1000);
+    }
+};
+
+const censor = (word, string) => {
+    const badWord = new RegExp(word, 'gi');
+    return string.replace(badWord, 'BLEEP');
+}
+
+const violenceCensor = (string) => {
+    return censor('violence', string);
+}
+
+
+const drugsCensor = (string) => {
+    return censor('drugs', string);
+}
