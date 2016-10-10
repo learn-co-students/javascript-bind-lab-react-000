906b2cf729e6eb1a75077966bf77c21679634a3d
talum <tracy.a.lum@gmail.com>
2016-10-09 20:38:04 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const app = "I don't do much.";
+
+const digitalClock = {
+  time: Math.round(Date.now()/1000),
+  startTicking: function() {
+    setInterval(function(){
+      this.time += 1
+    }.bind(this), 1000)
+  }
+};
+
+function censor(word, string){
+  const regex = new RegExp(word, "g");
+  return string.replace(regex, "BLEEP");
+}
+
+const violenceCensor = censor.bind(null, "violence");
+
+const drugsCensor = censor.bind(null, "drugs");
